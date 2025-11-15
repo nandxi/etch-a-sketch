d50509a94dd7d813761d0a3919f283168bb19b75
Nandan <230424156+nandxi@users.noreply.github.com>
2025-11-15 22:39:40 +0530

    Add function to remove divs from container

--- script.js
+++ script.js
@@ -2,21 +2,27 @@ let container = document.querySelector("#container")
 let button = document.querySelector("button")
 
 function addSquares(sides) {
-  for(let i = 0; i < sides; i++) {
-    for(let j = 0; j < sides; j++) {
-      let div = document.createElement("div")
+  for(let i = 0; i < sides * sides; i++) {
+    let div = document.createElement("div")
       div.classList.add("div")
       container.appendChild(div)
       div.style.width = `${500 / sides}px`
       div.style.height = `${500 / sides}px`
       div.addEventListener("mouseenter", () => {
         div.classList.add("colored")
-      })
-    }
+    })
+  }
+}
+
+function removeSquares(container) {
+  let divs = Array.from(document.querySelectorAll(".div"))
+  for (let div of divs) {
+    container.removeChild(div);
   }
 }
 
 button.addEventListener("click", () => {
   let squaresNo = +prompt("Enter the number of squares per side: ")
+  removeSquares(container)
   addSquares(squaresNo)
 })
